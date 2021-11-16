0047e045ae31d1b95a68887f4269d740c25ef423
Kojji <fernandokojiyama@gmail.com>
2021-11-16 19:58:53 -0400

    add: ordenação

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -67,8 +67,10 @@ const create = async (req, res) => {
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-    if (!emailRegex.test(email)) {
-      throw new APIError("Endereço de E-mail inválido.");
+    if (email !== '') {
+      if (!emailRegex.test(email)) {
+        throw new APIError("Endereço de E-mail inválido.");
+      }
     }
     
     const createdPatient = await Patient.create({
@@ -186,8 +188,8 @@ const update = async (req, res) => {
 
 // List patient forms
 const list = async (req, res) => {
-  const { limit = 20, page = 1, active=true, search=null } = req.query;
-  const offset = 0 + (parseInt(page) - 1) * limit;
+  const { limit = '20', page = 1, column='name', order='ASC', active=true, search=null } = req.query;
+  const offset = 0 + (parseInt(page) - 1) * parseInt(limit);
   try {
     let where = {}
     if (!!search) {
@@ -198,9 +200,16 @@ const list = async (req, res) => {
           },
           cpf: {
             [Op.iLike]: `%${search}%`
+          },
+          externalFile: {
+            [Op.iLike]: `%${search}%`
           }
         }
       }
+    }
+    let toOrder = [['name', order]]
+    if (column !== 'name') {
+      toOrder = [[column, order], ['name', 'ASC']]
     }
     where = {...where, active}
     const patients = await Patient.findAndCountAll({
@@ -208,8 +217,8 @@ const list = async (req, res) => {
       attributes: {
         exclude:['updatedAt']
       },
-      order: [['name', 'ASC']],
-      limit,
+      order: toOrder,
+      limit: parseInt(limit),
       offset
     })
     if(!patients)
